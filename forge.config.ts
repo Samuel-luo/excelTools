61274600214e49443de8f0523caaf4c0088681b4
Samuel-luo <1319433916@qq.com>
2022-12-05 14:05:30 +0800

    edit config

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,8 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+// import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
-import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
+// import { MakerDeb } from "@electron-forge/maker-deb";
+// import { MakerRpm } from "@electron-forge/maker-rpm";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
 import { mainConfig } from "./webpack.main.config";
@@ -23,9 +23,9 @@ const config: ForgeConfig = {
   rebuildConfig: {},
   makers: [
     // new MakerSquirrel({}),
-    new MakerZIP({}, ["darwin", "win32"]),
-    new MakerRpm({}),
-    new MakerDeb({})
+    new MakerZIP({})
+    // new MakerRpm({}),
+    // new MakerDeb({})
   ],
   plugins: [
     new WebpackPlugin({
